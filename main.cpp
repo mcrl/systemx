de2a02795a7cc4b7eaed758c0ff83aed28d7c590
gajagajago <gajagajago@naver.com>
2023-06-18 01:56:12 +0000

    spdlog

--- main.cpp
+++ main.cpp
@@ -3,16 +3,14 @@
 #include <iostream>
 #include <string>
 
+#include "spdlog/spdlog.h"
+
 #include "systemxConfig.hpp"
 #include "utils.hpp"
 
 int main(int argc, char* argv[])
 {
-
-  std::cout << SYSTEMX_NAME << " " << SYSTEMX_VERSION_MAJOR << "." << SYSTEMX_VERSION_MINOR << std::endl;
-
-  const double t = SYSTEMX::utils::gettime();
-  std::cout << "Time " << t << std::endl;
-
+  spdlog::info("{0}{1}.{2} ({3})", SYSTEMX_NAME, SYSTEMX_VERSION_MAJOR, SYSTEMX_VERSION_MINOR, SYSTEMX::utils::gettime());
+  
   return 0;
 }
