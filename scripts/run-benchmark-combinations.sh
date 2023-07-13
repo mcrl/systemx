558655135532f0627006e60a7e9aaf405d37ec5a
gajagajago <gajagajago@naver.com>
2023-07-13 05:51:27 +0000

    init scripts

--- scripts/run-benchmark-combinations.sh
+++ scripts/run-benchmark-combinations.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-# TARGET=aluCompute
+# Script to experiment all combinations of the benchmarks
+
 SYSX=~/systemx/build/systemx
 DIR=~/systemx/benchmarks
 
-for TARGET in aluCompute l2Load l2Store gmemLoad gmemStore
+for TARGET in aluCompute l1Load l1Store l2Load l2Store gmemLoad gmemStore pcieLoad pcieStore
 do
-  for BENCHMARK in aluCompute l2Load l2Store gmemLoad gmemStore
+  for BENCHMARK in aluCompute l1Load l1Store l2Load l2Store gmemLoad gmemStore pcieLoad pcieStore
   do
     ~/systemx/run-nsys.sh $SYSX --benchmark_file $DIR/serial/serial-$TARGET,$BENCHMARK.json; 
 
